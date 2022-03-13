459798337603e30d7589454069fb36165ce9c157
Carlos Alexandre Silva Genuino <carloxsgenuino3@live.com>
2022-03-12 22:32:48 -0300

    ribbon load balance

--- hr-worker/src/main/java/br/com/hrworker/resources/WorkerResource.java
+++ hr-worker/src/main/java/br/com/hrworker/resources/WorkerResource.java
@@ -2,7 +2,11 @@ package br.com.hrworker.resources;
 
 import br.com.hrworker.entities.Worker;
 import br.com.hrworker.repositories.WorkerRepository;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -16,6 +20,11 @@ import java.util.Optional;
 @RequestMapping(value = "/workers")
 public class WorkerResource {
 
+    private Logger logger = LoggerFactory.getLogger(WorkerResource.class);
+
+    @Autowired
+    private Environment env;
+
     @Autowired
     private WorkerRepository repository;
 
@@ -28,6 +37,8 @@ public class WorkerResource {
 
     @GetMapping("/{id}")
     public ResponseEntity<Worker> findById(@PathVariable("id") Long id) {
+        logger.info("Port = " + env.getProperty("local.server.port"));
+
         Optional<Worker> optionalWorker = repository.findById(id);
         return optionalWorker.map(ResponseEntity::ok).orElseGet(() -> ResponseEntity.notFound().build());
     }
