88a2c54b503752a975ebeeab639a76604364dc7a
Carlos Genuino <carloxsgenuino3@gmail.com>
2022-03-19 13:41:48 -0300

    adding resilience4j on HRPayroll

--- hr-payroll/src/main/java/br/com/genuino/hrpayroll/resources/PaymentResource.java
+++ hr-payroll/src/main/java/br/com/genuino/hrpayroll/resources/PaymentResource.java
@@ -2,7 +2,10 @@ package br.com.genuino.hrpayroll.resources;
 
 import br.com.genuino.hrpayroll.entities.Payment;
 import br.com.genuino.hrpayroll.services.PaymentService;
+import com.netflix.ribbon.proxy.annotation.Hystrix;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -15,10 +18,18 @@ public class PaymentResource {
 
     @Autowired
     private PaymentService service;
+    private static final String PAYMENT_FALLBACK = "paymentFallback";
+
 
     @GetMapping("/{workerId}/days/{days}")
+    @CircuitBreaker(name = PAYMENT_FALLBACK, fallbackMethod = "getPaymentAlternative")
     public ResponseEntity<Payment> getPayment(@PathVariable("workerId") Long id, @PathVariable("days") Integer days ){
         Payment payment = service.getPayment(id, days);
+        return  ResponseEntity.ok(payment);
+    }
+
+    public ResponseEntity<Payment> getPaymentAlternative(Long id, Integer days, Throwable t ){
+        Payment payment = new Payment("Brann", 400.0, days);
         return  ResponseEntity.ok(payment);
     }
 }
